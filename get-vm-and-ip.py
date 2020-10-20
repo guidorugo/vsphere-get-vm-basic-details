97e8c5d15b47d3c86de9414271da0bc600a1c00e
Guido Rugo <guido.rugo@microfocus.com>
2020-10-20 19:39:16 +0100

    First commit, stable version

---
+++ get-vm-and-ip.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+__author__ = 'Guido Rugo guido.rugo@microfocus.com'
+
+import atexit
+import ssl
+import requests
+import urllib3
+import argparse
+import getpass
+import re
+from pyVim import connect
+from pyVmomi import vim
+
+parser = argparse.ArgumentParser(description='Standard arguments for talking to vCenter.')
+required_parser = parser.add_argument_group('Required arguments')
+required_parser.add_argument('-s', '--server', action='store', required=True, help='vSphere service hostname or IP to connect to.')
+required_parser.add_argument('-u', '--username', action='store', required=True, help='Username to use when connecting to vc.')
+parser.add_argument('-f', '--filter', action='store', help='Filter if contains in name.')
+args = parser.parse_args()
+
+session = requests.session()
+session.verify = False
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+context = None
+if hasattr(ssl, '_create_unverified_context'):
+    context = ssl._create_unverified_context()
+password = getpass.getpass('Password for '+args.username+': ')
+
+service_instance = connect.SmartConnect(host=args.server, user=args.username, pwd=password, sslContext=context)
+
+# atexit.register(connect.Disconnect, service_instance)
+
+content = service_instance.RetrieveContent()
+
+container = content.rootFolder
+viewType = [vim.VirtualMachine]
+containerView = content.viewManager.CreateContainerView(container, viewType, True)
+children = containerView.view
+print('\n')
+for child in children:
+    summary = child.summary
+    if args.filter:
+        if args.filter in summary.config.name:
+            print('Name: '+summary.config.name)
+            if summary.guest.ipAddress is None:
+                print('IP: VM Shutdown')
+            else:
+                print('IP: '+summary.guest.ipAddress)
+            print('ID: '+re.findall('\d+', str(summary.vm))[0])
+            print('OS: '+summary.config.guestFullName)
+            print('--------------------------------------')
+    else:
+        print('Name: '+summary.config.name)
+        if summary.guest.ipAddress is None:
+            print('IP: VM Shutdown')
+        else:
+            print('IP: '+summary.guest.ipAddress)
+        print('ID: '+re.findall('\d+', str(summary.vm))[0])
+        print('OS: '+summary.config.guestFullName)
+        print('--------------------------------------')
